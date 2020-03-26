10bf416f0a6f7f440ac0ec48a487b36917628887
sushaoci <sushaociqiqi@gmail.com>
2020-03-26 15:00:20 +0800

    invi

--- Frontend/src/components/zoomcircle/Zoomable.vue
+++ Frontend/src/components/zoomcircle/Zoomable.vue
@@ -3,7 +3,7 @@
 </template>
 <script>
 import * as d3 from "d3";
-import testdata from './data.json';
+import testdata from "./data.json";
 
 let width = window.innerWidth;
 let height = window.innerHeight;
@@ -26,9 +26,9 @@ const pack = data =>
 export default {
   data() {
     return {
-      svg:{},
-      node:{},
-      lable:{},
+      svg: {},
+      node: {},
+      lable: {},
       data: {},
       root: {},
       focus: {},
@@ -46,26 +46,26 @@ export default {
         .style("background", color(0))
         .style("cursor", "pointer")
         .on("click", () => this.zoom(this.root));
-      
+
       this.node = this.svg
         .append("g")
         .selectAll("circle")
         .data(this.root.descendants().slice(1))
         .join("circle")
-        .attr("fill", d => (d.children ? color(d.depth) : "white"))
+        // .attr("fill", d => (d.children ? color(d.depth) : "white"))
+        .attr("fill", color(1))
         .attr("pointer-events", d => (!d.children ? "none" : null))
-        .on("mouseover", function() {
-          d3.select(this).attr("stroke", "#000");
-        })
-        .on("mouseout", function() {
-          d3.select(this).attr("stroke", null);
-        })
-        .on("click", d => focus !== d && (this.zoom(d), d3.event.stopPropagation()));
+        .style("fill-opacity", d => (d.parent === this.root ? 1 : 0))
+        .style("display", d => (d.parent === this.root ? "inline" : "none"))
+        .on(
+          "click",
+          d => focus !== d && (this.zoom(d), d3.event.stopPropagation())
+        );
 
       this.label = this.svg
         .append("g")
         .style("font", "18px Charlemagne Std")
-        .attr("fill",'#000')
+        .attr("fill", "#000")
         .attr("pointer-events", "none")
         .attr("text-anchor", "middle")
         .selectAll("text")
@@ -81,19 +81,22 @@ export default {
       this.focus = this.root;
     },
     zoomTo(v) {
-      const k = v[0]<v[1] ? width / v[2] : height / v[2];
+      const k = v[0] < v[1] ? width / v[2] : height / v[2];
 
       this.view = v;
 
-      this.label.attr(
-        "transform",
-        d => d.x && d.y ? `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`:'translate( 0, 0 )'
+      this.label.attr("transform", d =>
+        d.x && d.y
+          ? `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
+          : "translate( 0, 0 )"
       );
-      this.node.attr(
-        "transform",
-        d => d.x && d.y ? `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`:'translate( 0, 0 )'
+      this.node.attr("transform", d =>
+        d.x && d.y
+          ? `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`
+          : "translate( 0, 0 )"
       );
       this.node.attr("r", d => d.r * k);
+      this.node.classed("faded", false);
     },
     zoom(d) {
       const focus0 = focus;
@@ -104,11 +107,28 @@ export default {
         .transition()
         .duration(d3.event.altKey ? 7500 : 750)
         .tween("zoom", d => {
-          const i = d3.interpolateZoom(this.view, [focus.x, focus.y, focus.r * 2]);
+          const i = d3.interpolateZoom(this.view, [
+            focus.x,
+            focus.y,
+            focus.r * 2
+          ]);
           return t => this.zoomTo(i(t));
         });
 
       this.label
+        .filter(function(d) {
+          return d.parent === focus || this.style.display === "inline";
+        })
+        .transition(transition)
+        .style("fill-opacity", d => (d.parent === focus ? 1 : 0))
+        .on("start", function(d) {
+          if (d.parent === focus) this.style.display = "inline";
+        })
+        .on("end", function(d) {
+          if (d.parent !== focus) this.style.display = "none";
+        });
+
+      this.node
         .filter(function(d) {
           return d.parent === focus || this.style.display === "inline";
         })
